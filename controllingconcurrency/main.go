6b853e06bcbf62517d7b9c2c0f0a537272253bd5
sashank45 <tadimetiramakrishnasashank.21.cse@anits.edu.in>
2025-12-16 22:35:46 +0530

    go concurrency fix

---
+++ controllingconcurrency/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+const (
+	concurrencyLevel = 3
+)
+
+var (
+	requestIds = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+)
+
+func main() {
+	queue := make(chan bool, concurrencyLevel)
+	var wg sync.WaitGroup
+	for _, id := range requestIds {
+		queue <- true
+		wg.Add(1)
+		go func(id int) {
+			defer func() {
+				<-queue
+			}()
+			defer wg.Done()
+			makerequest(id)
+		}(id)
+	}
+	wg.Wait()
+	// for i:=0;i<concurrencyLevel;i++ { 
+	// 	queue<-true
+	// }  fix with a for loop
+}
+func makerequest(id int) {
+	time.Sleep(3 * time.Second)
+	fmt.Println(id)
+}
