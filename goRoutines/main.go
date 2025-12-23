d8f076eab58dbe6d762f15eec7e437d9eedfffbe
sashank45 <tadimetiramakrishnasashank.21.cse@anits.edu.in>
2025-12-23 20:57:37 +0530

    fix goroutines folder

--- goRoutines/main.go
+++ goRoutines/main.go
@@ -2,20 +2,23 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
 	ch := make(chan int)
-
+	var wg sync.WaitGroup
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		fmt.Println("Before sending 5")
 		ch <- 5 
 		time.Sleep(2 * time.Second)
 		fmt.Println("After sending 5")
 	}()
-
 	val := <-ch
+	wg.Wait()
 	fmt.Println("Received:", val)
 
 }
